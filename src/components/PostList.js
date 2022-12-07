4d8b3ef28b6e0bb33322d2c344246626e076ab4e
Akash RP <akashrp512@gmail.com>
2022-12-07 21:03:28 +0530

    commit

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from "../actions/index"
 
 class PostList extends React.Component {
+
+componentDidMount() {
+    this.props.fetchPosts();
+}
+
     render() {
         return <div className='ui header'>Post List</div>
     }
 }
 
-export default PostList;
+export default connect(null, {fetchPosts}) (PostList);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers/index';
+import thunk from 'redux-thunk';
 
+const store =createStore(reducers, applyMiddleware(thunk));
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
+  
   <React.StrictMode>
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   </React.StrictMode>
